daeb74e25bf3e0db3501cb896ebb95343b88c41f
l00907l <157943579+l00907l@users.noreply.github.com>
2026-04-24 14:03:04 +0800

    Add files via upload

---
+++ DiPaF/scripts/VQVAE/electricty.sh
@@ -0,0 +1,28 @@
+seq_len=96
+
+root_path_name=../dataset/electricity
+data_path_name=electricity.csv
+model_id_name=electricity
+data_name=custom
+ckpt_path=./ckpt_vqvae
+num_features=321
+
+
+python -u run_vae.py \
+    --data $data_name \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --features M \
+    --checkpoints $ckpt_path \
+    --num_features $num_features \
+    --seq_len $seq_len \
+    --enc_hidden_dim 256 \
+    --embed_dim 64 \
+    --dec_hidden_dim 256 \
+    --n_clusters 30 \
+    --patch_len 16 \
+    --stride 8 \
+    --model_id $model_id_name \
+    --train_epochs 12 \
+    --lr 0.01 \
+    --batch_size 32
